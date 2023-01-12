2cdb5e9e68d6ba7759870800c3ecef1163f228b5
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-12 18:06:54 +0200

    corrected homework

--- ComplexCycles.js
+++ ComplexCycles.js
@@ -1,75 +1,73 @@
+"use strict";
 //Вивести числа від 20 до 30 через пропуск, використовуючи
 // крок 0,5 (20 20,5 21 21,5….).
-let arr = []
-let result = []
+const result = []
 for (let i = 20 ; i <= 30 ;i = i + 0.5) {
-
-    for (let j = 0; i <= 30 ; j++) {
-        arr[j] = i
-        result.push(arr[j])
-        i = i + 0.5
-    }
+        result.push(i)
 }
 alert(result.join(' '))
 /**************************************************************/
 //Один долар коштує 27 гривень. Вивести дані з розрахунком
 // вартості 10, 20, 30... 100 доларів.
-let item = []
-let table = []
+const table = []
 for (let q = 10 ; q <= 100 ;q = q + 10) {
-    for (let r = 0; q <= 100 ; r++) {
-        item[r] = `${q}$ = ${q*27}  \n`
-        table.push(item[r])
-        q = q + 10
-    }
+    table.push(`${q}$ = ${q*27}  \n`)
 }
 alert(table.join(' '))
 /*****************************************************************/
 // Дане ціле число. Вивести всі цілі числа від 1 до 100,
 // квадрат яких не перевищує числа N.
-const n = Number(prompt('Enter number-limiter :'))
-let element = []
-let arrIntegers = []
-for (let t = 0; t <= 100; t++) {
-    for (let p = 0; t <= 100 ; p++) {
-        if (t*t < n) {
-            element[p] = t
-            arrIntegers.push(element[p])
+const integerNumberOfUser = Number(prompt('Enter number-limiter :'))
+const arrIntegers = []
+if (integerNumberOfUser > 0) {
+    for (let t = 0; t <= 100; t++) {
+        if (t*t < integerNumberOfUser) {
+            arrIntegers.push(t)
         }
-        break
     }
+    alert(arrIntegers.join(' '))
 }
-alert(arrIntegers.join(' '))
+else {
+    alert(`Number is invalid`)
+}
 /************************************************************/
 // Дане ціле число. З'ясувати, чи є воно простим(простим називається число,
 // більше 1, які не мають інших дільників крім 1 і себе).
-const w = Number(prompt('Is your number a Prime number ? :'))
+const numberToCheck = Number(prompt('Is your number a Prime number ? :'))
 let massages
-for (let a = 2; a < w ; a++) {
-    if ((w % a) === 0) {
-        massages = "It is not Prime number"
-        break
+if (numberToCheck > 1) {
+    for (let a = 2; a < numberToCheck; a++) {
+        if ((numberToCheck % a) === 0) {
+            massages = "It is not Prime number"
+            break
+        } else {
+            massages = "It is Prime number"
+        }
     }
-    else {
-        massages = "It is Prime number"
-    }
+    alert(`${numberToCheck} : ${massages} `)
 }
-alert(`${w} : ${massages} `)
+else {
+    alert(`Number is invalid`)
+}
 /***************************************************************/
 //Дане деяке число. Визначити, чи можна одержати це числo
 // шляхом зведення числа 3 у деякий ступінь.
 // (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
-const f = Number(prompt('Enter a number to be checked :'))
-let answer
-let l = 0
-for (let b = 1; b <= f ; b++) {
-    if (f % (3**b) === 0) {
-        answer = 'Yes'
-        l++
-        break
+const numberToChecked = Number(prompt('Enter a number to be checked :'))
+if (numberToChecked > 1) {
+    let answer
+    let exponentiation = 0
+    for (let b = 1; b <= numberToChecked; b++) {
+        if (numberToChecked % (3 ** b) === 0) {
+            answer = 'Yes'
+            exponentiation++
+            break
+        } else {
+            answer = 'No'
+        }
     }
-    else {
-        answer = 'No'
-    }
+    alert(`${answer}, it exponentiation is : ${exponentiation}`)
 }
-alert(`${answer}, it exponentiation is : ${l}`)
+else {
+    alert(`Number is invalid`)
+}
